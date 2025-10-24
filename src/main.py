1d1cd08b0f18a23b1ca439557727b5ee58beb029
thiagofernandes101 <thiagofernandes101@outlook.com>
2025-10-23 22:36:50 -0300

    Refatoração para implementar o uso do módulo dnn do opencv
    - Houve melhora considerável na detecção de rostos em relação ao modelo anterior sem retornar muitos falsos positivos no decorrer do vídeo, mas ainda alguns falsos negativos, provavelmente devido ao seus dados de treino

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import cv2 as cv
+import numpy as np
 import os
 
 script_directory = os.path.dirname(__file__)
 video_path = '/home/thiagofernandes101/projects/fiap/analise-video/videos/Unlocking Facial Recognition_ Diverse Activities Analysis.mp4'
+proto_path = os.path.join(script_directory, "deploy.prototxt")
+model_path = os.path.join(script_directory, "res10_300x300_ssd_iter_140000.caffemodel")
+net = cv.dnn.readNetFromCaffe(proto_path, model_path)
 
 video_capture = cv.VideoCapture(video_path)
-
-face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = video_capture.read()
@@ -15,11 +17,23 @@ while True:
         print("End of video stream or cannot read the video.")
         break
     
-    gray_scale_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_scale_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    (h, w) = frame.shape[:2]
+    blob = cv.dnn.blobFromImage(
+        cv.resize(frame, (300, 300)), 
+        1.0,
+        (300, 300), 
+        (104.0, 177.0, 123.0)
+    )
     
-    for (x, y, w, h) in faces:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    net.setInput(blob)
+    detections = net.forward()
+    
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            cv.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
         
     cv.imshow('Face Detection on Video', frame)
     
